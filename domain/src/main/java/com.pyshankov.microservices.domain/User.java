040c0cbff194d6748db0ed8b092cbc1122b44b41
pyshankov <pyshankov@gmail.com>
2018-04-30 00:25:18 +0300

    add auth

--- domain/src/main/java/com.pyshankov.microservices.domain/User.java
+++ domain/src/main/java/com.pyshankov.microservices.domain/User.java
@@ -1,11 +1,29 @@
 package com.pyshankov.microservices.domain;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-public class User {
+@NoArgsConstructor
+@Data
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -134343434L;
+
+    @Id
     private String id;
+    @Indexed(unique = true)
     private String email;
     private String password;
     private BigDecimal amount;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
