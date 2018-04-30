78423ae5b4f5d48ec46a62920a9dfaf5c962d9d5
Alexey Zatsepin <alexzatsepin7@gmail.com>
2018-04-30 22:06:20 +0300

    add product service

--- domain/src/main/java/com.pyshankov.microservices.domain/Order.java
+++ domain/src/main/java/com.pyshankov.microservices.domain/Order.java
@@ -1,5 +1,8 @@
 package com.pyshankov.microservices.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -7,10 +10,13 @@ import java.util.List;
 /**
  * Created by pyshankov on 4/29/18.
  */
-public class Order {
+
+@NoArgsConstructor
+@Data public class Order {
     private String id;
     private LocalDateTime createDate;
     private List<Product> products;
     private BigDecimal price;
+    private User user;
 
 }
--- product-service/src/main/java/com/pyshankov/microservices/service/ProductService.java
+++ product-service/src/main/java/com/pyshankov/microservices/service/ProductService.java
@@ -1,24 +1,34 @@
 package com.pyshankov.microservices.service;
 
+import com.pyshankov.microservices.domain.Product;
+import com.pyshankov.microservices.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class ProductService {
 
-    public void createProduct() {
+    @Autowired
+    ProductRepository productRepository;
 
+    public void createProduct(String name, String description, BigDecimal price) {
+        Product product = new Product(name, description, price);
+        productRepository.save(product);
     }
 
-    public void deleteProduct() {
-
+    public void deleteProduct(Long id) {
+        productRepository.delete(id);
     }
 
-    public void getProductDetails(String productId) {
-
+    public Product getProduct(Long id) {
+        return productRepository.findOne(id);
     }
 
-    public void getAllProducts() {
-
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 
 
