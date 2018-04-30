c09dc06981dfa49ad99a36fa1e5702022ec19b74
Alexey Zatsepin <alexzatsepin7@gmail.com>
2018-04-30 23:19:40 +0300

    add controller

--- product-service/src/main/java/com/pyshankov/microservices/service/ProductService.java
+++ product-service/src/main/java/com/pyshankov/microservices/service/ProductService.java
@@ -1,12 +1,12 @@
 package com.pyshankov.microservices.service;
 
 import com.pyshankov.microservices.domain.Product;
+import com.pyshankov.microservices.domain.ProductEventType;
 import com.pyshankov.microservices.repository.ProductRepository;
 import com.pyshankov.microservices.domain.ProductEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -18,17 +18,18 @@ public class ProductService {
     @Autowired
     private AmqpProducerService amqpProducerService;
 
-    public void createProduct(String name, String description, BigDecimal price) {
-        Product product = new Product(name, description, price);
+    public void persist(Product product) {
         productRepository.save(product);
-        amqpProducerService.produceMsg(new ProductEvent());
+        amqpProducerService.produceMsg(new ProductEvent(product.getId(), ProductEventType.CREATE_PRODUCT));
     }
 
     public void deleteProduct(Long id) {
         productRepository.delete(id);
+        amqpProducerService.produceMsg(new ProductEvent(id, ProductEventType.DELETE_PRODUCT));
     }
 
     public Product getProduct(Long id) {
+        amqpProducerService.produceMsg(new ProductEvent(id, ProductEventType.VIEW_PRODUCT));
         return productRepository.findOne(id);
     }
 
