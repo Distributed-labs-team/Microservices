a4f6070b4adcfb78291d6710a6e8a3f6a1d3b698
pyshankov <pyshankov@gmail.com>
2018-05-06 18:39:23 +0300

    fix hazelcast-cache

--- product-service/src/main/java/com/pyshankov/microservices/service/ProductService.java
+++ product-service/src/main/java/com/pyshankov/microservices/service/ProductService.java
@@ -1,11 +1,9 @@
 package com.pyshankov.microservices.service;
 
 import com.pyshankov.microservices.domain.Product;
-import com.pyshankov.microservices.domain.ProductEventType;
 import com.pyshankov.microservices.domain.User;
 import com.pyshankov.microservices.hazelcast.cache.HazelcastClientTemplate;
 import com.pyshankov.microservices.repository.ProductRepository;
-import com.pyshankov.microservices.domain.ProductEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,20 +21,21 @@ public class ProductService {
     @Autowired
     private HazelcastClientTemplate hazelcastClientTemplate;
 
-    public void persist(Product product, String token) {
+    public Product persist(Product product, String token) {
         User user = hazelcastClientTemplate.getUserFromCacheByToken(token);
         product.setOwner(user.getEmail());
-        productRepository.save(product);
-        amqpProducerService.produceMsg(new ProductEvent(product.getId(),null, ProductEventType.CREATE_PRODUCT));
+        Product product1 = productRepository.save(product);
+//        amqpProducerService.produceMsg(new ProductEvent(product.getId(),null, ProductEventType.CREATE_PRODUCT));
+        return product1;
     }
 
     public void deleteProduct(String id) {
         productRepository.delete(id);
-        amqpProducerService.produceMsg(new ProductEvent(id,null, ProductEventType.DELETE_PRODUCT));
+//        amqpProducerService.produceMsg(new ProductEvent(id,null, ProductEventType.DELETE_PRODUCT));
     }
 
     public Product getProduct(String id) {
-        amqpProducerService.produceMsg(new ProductEvent(id,null, ProductEventType.VIEW_PRODUCT));
+//        amqpProducerService.produceMsg(new ProductEvent(id,null, ProductEventType.VIEW_PRODUCT));
         return productRepository.findOne(id);
     }
 
