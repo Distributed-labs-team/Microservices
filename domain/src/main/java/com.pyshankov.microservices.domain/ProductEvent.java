2d88b7d489dc8aea502ce27f80ccc6318a70a3f2
Artemenko Valerii <vatvalusha@gmail.com>
2018-05-21 23:32:54 +0300

    add @table for Product

--- domain/src/main/java/com.pyshankov.microservices.domain/ProductEvent.java
+++ domain/src/main/java/com.pyshankov.microservices.domain/ProductEvent.java
@@ -1,11 +1,16 @@
 package com.pyshankov.microservices.domain;
 
+import org.springframework.data.cassandra.mapping.PrimaryKey;
+import org.springframework.data.cassandra.mapping.Table;
+
 import java.time.LocalDateTime;
 
+@Table("ProductEvent")
 public class ProductEvent extends Event {
 
     private static final long serialVersionUID = -4223434L;
 
+    @PrimaryKey
     private String productId;
     private ProductEventType productEventType;
     private LocalDateTime timestamp;
